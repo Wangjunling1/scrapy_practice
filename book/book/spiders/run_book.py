b21706c7bcea1f0e5021026cd1d1b50d97d641b6
junling.wang <junling.wang@lejent.com>
2019-02-26 19:21:26 +0800

    修改下一页思路

--- book/book/spiders/run_book.py
+++ book/book/spiders/run_book.py
@@ -8,9 +8,10 @@ class Blues(scrapy.Spider):
     start_urls=['https://www.lwxslwxs.com/51/51339/']
     def parse(self, response):
         item = BookItem()
+        item["book_text_next"] = ''
+
         item['book_name']=response.xpath('//div[@class="info2"]/h1[@class=" text-center"]/text()').extract_first()
         datas=response.xpath('//div[@class="row"][3]/div[@class="col-sm-12 col-md-12"]/div[@class="panel panel-default"]/div[@class="panel-body"]/ul/li')
-        item['book_text']=''
         print(item['book_name'])
         for data in datas:
             item["data_url"]=data.xpath('a/@href').extract_first()
@@ -19,19 +20,26 @@ class Blues(scrapy.Spider):
     def content(self,response):
         item = response.meta['data']
         item['book_title'] = response.xpath('//div[@class="panel-heading"][1]/text()').extract_first()
-        text=','.join(response.xpath('//div[@class="panel-body content-body content-ext"]/text()').extract())
-        item['book_text']=item['book_text']+text
-        net=response.xpath('//li[@class="next"]/a[@class="btn btn-info"]/@href').extract_first()
-        if len(net)>8:
-            if net[:-7] in str(response):
-                print(net)
-                print('下一页')
-                yield scrapy.Request(url='https://www.lwxslwxs.com'+net,callback=self.content,meta={'data':item})
-            else:
-                yield item
-                print('True,存入',item['book_title'])
-        else:
+        item['book_text'] = ''
+        for i in range(3):
+            page=os.path.split(response.url)[-1][:-5]+'-{}.html'.format(i)
+            url=os.path.split(response.url)[0]+'/'+page
+            yield scrapy.Request(url=url,callback=self.next,meta={'data':item})
+
+    def next(self,response):
+        print(response)
+        item = response.meta['data']
+        net = response.xpath('//li[@class="next"]/a[@class="btn btn-info"]/@href').extract_first()
+        text = ','.join(response.xpath('//div[@class="panel-body content-body content-ext"]/text()').extract())
+        if  item["book_text_next"] != net:
+            item["book_text_next"]=net
+            item["book_text"]=item["book_text"]+text
+        if item["book_text_next"] == net:
+            print(item["book_text"])
             yield item
+
+
+
 
 
 
--- book/book/spiders/实验.py
+++ book/book/spiders/实验.py
@@ -3,11 +3,11 @@ from scrapy.linkextractor import LinkExtractor
 
 class WeidsSpider(scrapy.Spider):
     name = "weids"
-    allowed_domains = ["www.wln100.com"]
-    start_urls = ['https://www.wln100.com/Doc/Doc/show/grade/0/area/0/tid/0/times/0/year/0/sid/12']
+    allowed_domains = ["www.lwxslwxs.com"]
+    start_urls = ['https://www.lwxslwxs.com/51/51339/']
 
     def parse(self, response):
-        link = LinkExtractor(restrict_xpaths='//td[@class="sjtit"]/a')
+        link = LinkExtractor(restrict_xpaths='//div[@class="row"][3]/div[@class="col-sm-12 col-md-12"]/div[@class="panel panel-default"]/div[@class="panel-body"]/ul[@class="list-group list-charts"]/li/a')
         links = link.extract_links(response)
         print(link)
         print(response)
